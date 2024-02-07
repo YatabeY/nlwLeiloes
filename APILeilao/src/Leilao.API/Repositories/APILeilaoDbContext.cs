f01c248184b9d72296e972891f5f74a8fc3af2f1
Anthony Yatabe <anthonypiresdm@gmail.com>
2024-02-07 16:30:50 -0300

    criando controllers, etities, bd

---
+++ APILeilao/src/Leilao.API/Repositories/APILeilaoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Leilao.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Leilao.API.Repositories;
+
+public class APILeilaoDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+
+    //Onde ta o banco de dados
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(@"Data Source=‪D:\APIleilao\leilaoDbNLW.db");
+    }
+}
---
+++ APILeilao/src/Leilao.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionsUseCase.cs
@@ -0,0 +1,20 @@
+﻿using Leilao.API.Entities;
+using Leilao.API.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Leilao.API.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionsUseCase
+{
+    public Auction? Execute()
+    {
+        var repository = new APILeilaoDbContext();
+
+        var today = DateTime.Now;
+
+        return repository
+            .Auctions
+            .Include(auction => auction.Items)
+            .FirstOrDefault(auction => today >= auction.Starts && today <= auction.Ends);
+    }
+}
